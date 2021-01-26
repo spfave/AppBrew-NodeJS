e52a9627010e9200cbe1e26e74208cbd837d23ee
Sebastian F <sebpfave2@gmail.com>
2021-01-25 20:19:33 -0500

    External Node.JS module

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Course: The Complete Web Development Bootcamp
 Course url: https://www.appbrewery.co/courses
 
 ## Section 18 - Node.JS
+
+- REPL: Read Evaluation Print Loop
+- using native Node modules
+- Installing external Node Modules
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const fs = require("fs");
+const superheroes = require("superheroes");
+const supervillains = require("supervillains");
 
-fs.copyFileSync("file1.txt", "file2.txt");
+let mySuperheroName = superheroes.random();
+console.log(mySuperheroName);
+
+let mySupervillainName = supervillains.random();
+console.log(mySupervillainName);
